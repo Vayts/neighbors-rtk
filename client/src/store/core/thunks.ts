e57a78527550dc471da0c2a2132a11a3f6032e19
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-10-20 11:43:32 +0300

    fixed bug with neighborhoods request without user;

--- client/src/store/core/thunks.ts
+++ client/src/store/core/thunks.ts
@@ -5,15 +5,21 @@ import { refresh } from '@src/store/auth/thunks';
 import { ILocale } from '@src/types/locale.types';
 import { setLanguage } from '@src/store/core/slice';
 import { getUserNeighborhoods } from '@src/store/userNeighborhoods/thunks';
+import { RootState } from '@src/store';
 
 const MODULE_NAME = 'core';
 
 export const appFirstLoad = createAsyncThunk(
   `${MODULE_NAME}/firstLoad`,
-  async (_, { dispatch }) => {
+  async (_, { dispatch, getState }) => {
     try {
       await dispatch(refresh());
-      await dispatch(getUserNeighborhoods());
+      
+      const user = (getState() as RootState).auth.user;
+      
+      if (user) {
+        await dispatch(getUserNeighborhoods());
+      }
       
       const locale = window.localStorage.getItem('neighbors_lang');
       
